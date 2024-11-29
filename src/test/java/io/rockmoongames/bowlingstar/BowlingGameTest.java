f9ce9dec4ebf166b9607f975e1cd056063145b6e
tatunik <tunik.tatiana@gmail.com>
2024-11-29 13:56:49 -0800

    Add a new test class

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -7,9 +7,15 @@ public class BowlingGameTest {
     @Test
     public void jessicaTatianaTestOne() {
 
-    }
+   }
     @Test
     public void jessicaTatianaTestTwo() {
 
-    }
+    } 
+
+}
+@Test
+public void new-test-class() {
+
+} 
 }
